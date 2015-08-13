96ea39d6bac55cd461c8625eaa08d066d72251cd
Eric Martindale <eric@ericmartindale.com>
2015-08-13 13:39:13 -0700

    Add docs, working directory rendering.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # maki-cms-local
 file-based CMS for Maki
+
+## Quick Start
+```javascript
+var Maki = require('maki');
+var app = new Maki();
+
+var CMS = require('maki-cms-local');
+var cms = new CMS({
+  base: '/docs', // top-level URL base. becomes: yourserver.com/docs
+  path: 'docs/', // local directory to serve from.  Markdown-formatted.
+  view: 'docs/'  // absolute path to the Jade view to utilize.
+});
+
+app.use(cms);
+app.start();
+```
+
+All config values are optional, and default to sane values:
+```
+{
+  base: '/pages',
+  path: process.env.PWD + '/pages', // looks for `./pages` in your app folder
+  view: __dirname + '/../views/page.jade' // looks for `./views/page.jade` in your app folder
+}
+```
--- src/cms.js
+++ src/cms.js
@@ -10,7 +10,8 @@ function CMS(options) {
 
   self.config = {
     base: options.base || '/pages',
-    path: options.path || process.env.PWD + '/pages'
+    path: options.path ? process.env.PWD + options.path : process.env.PWD + '/pages',
+    view: options.view || __dirname + '/../views/page.jade'
   };
   self.extends = {
     services: {
@@ -19,13 +20,21 @@ function CMS(options) {
           return next();
         },
         setup: function(maki) {
-          maki.app.use(self.config.base + '/:filePath', function(req, res, next) {
-            var localPath = self.config.path + '/' + req.param('filePath') + '.md';
-            if (!fs.existsSync(localPath)) {
+          maki.app.use(self.config.base + '/:filePath?', function(req, res, next) {
+            var filePath = req.param('filePath');
+            var indexPath = self.config.path + '/index.md';
+            var localPath = self.config.path + '/' + filePath + '.md';
+
+            var path;
+            if (fs.existsSync(localPath)) {
+              path = localPath;
+            } else if (fs.existsSync(indexPath)) {
+              path = indexPath;
+            } else {
               return next();
             }
 
-            var input = fs.readFileSync(localPath);
+            var input = fs.readFileSync(path);
             var front = frontMatter.loadFront(input);
             var rendered = marked(front.__content);
 
@@ -34,7 +43,9 @@ function CMS(options) {
                 res.send(front);
               },
               html: function() {
-                res.send(rendered);
+                res.render(self.config.view, {
+                  content: rendered
+                });
               }
             });
           });
