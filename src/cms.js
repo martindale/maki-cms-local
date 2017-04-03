9171c9a20aa3f1aece9ed10d330ab376d88c1476
Eric Martindale <eric@ericmartindale.com>
2017-04-03 01:18:00 -0700

    Add and extend locals with metadata

--- src/cms.js
+++ src/cms.js
@@ -7,24 +7,43 @@ var _ = require('lodash');
 function CMS(options) {
   var self = this;
   if (!options) {
-    options = {};
+    options = {
+      components: {}
+    };
   }
+  
+  if (!options.components) options.components = {};
 
   self.config = {
     base: options.base || '/pages',
     path: options.path ? process.env.PWD + options.path : process.env.PWD + '/pages',
     view: options.view || __dirname + '/../views/page.jade'
   };
-  self.provides = {
-    Page: {
-      public: false,
-      internal: true,
-      attributes: {
-        name: { type: String , required: true , slug: true },
-        content: { type: String }
-      }
+
+  self.provides = {};
+  
+  var provider = options.name || 'Page'
+  self.provides[provider] = {
+    public: options.public || false,
+    name: options.name || 'Page',
+    icon: options.icon || 'book',
+    description: options.description || 'Static page.',
+    source: options.source,
+    routes: {
+      query: self.config.base,
+      get: self.config.base + '/:filePath'
+    },
+    attributes: {
+      name: { type: String , required: true },
+      content: { type: String }
+    },
+    components: {
+      masthead: options.components.masthead || 'maki-pitch',
+      query: options.components.query || 'maki-page-list',
+      get: options.components.get || 'maki-page-view'
     }
   };
+
   self.extends = {
     services: {
       http: {
@@ -32,15 +51,15 @@ function CMS(options) {
           return next();
         },
         setup: function(maki) {
+          var files = fs.readdirSync(self.config.path);
           var route = self.config.base + '/:filePath?';
-          maki.routes[route] = 'Page';
-
+          maki.routes[route] = provider;
           maki.app.use(route, function(req, res, next) {
             var filePath = req.param('filePath');
             var indexPath = self.config.path + '/index.md';
             var localPath = self.config.path + '/' + filePath + '.md';
+            var path = null;
 
-            var path;
             if (fs.existsSync(localPath)) {
               path = localPath;
             } else if (fs.existsSync(indexPath)) {
@@ -53,22 +72,20 @@ function CMS(options) {
             var front = frontMatter.loadFront(input);
             var rendered = marked(front.__content);
 
+            var locals = {
+              content: rendered,
+              metadata: front,
+              page: front
+            };
+            
+            locals = _.extend(locals, front);
+
             res.format({
               json: function() {
-                res.send(front);
+                res.send(locals);
               },
               html: function() {
-
-                var locals = {
-                  content: rendered,
-                  metadata: front,
-                  page: front
-                };
-
-                locals = _.extend(locals, front);
-
                 res.render(self.config.view, locals);
-
               }
             });
           });
